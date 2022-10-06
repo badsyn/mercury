621707135105a51eb06de9631ecd2282430183e0
badsyn <badsyn@oaciq.com>
2022-10-06 11:49:23 -0400

    Update mercury_controller.rb
    Rails 5.1

--- app/controllers/mercury_controller.rb
+++ app/controllers/mercury_controller.rb
@@ -6,7 +6,7 @@ class MercuryController < ActionController::Base
   layout false
 
   def edit
-    render :text => '', :layout => 'mercury'
+    render :plain => '', :layout => 'mercury'
   end
 
   def resource
@@ -23,7 +23,7 @@ class MercuryController < ActionController::Base
   end
 
   def test_page
-    render :text => params
+    render :plain => params
   end
 
   private
